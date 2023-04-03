ead82f5cd533b3df7e47cfef445975c8e7112dfe
august95 <ingvar.wedervang@gmail.com>
2023-04-03 17:44:30 +0200

    created token structure

--- compiler.h
+++ compiler.h
@@ -2,6 +2,50 @@
 #define  CCOMPIELR_H
 
 #include <stdio.h>
+#include <stdbool.h>
+
+struct pos
+{
+    int line;
+    int col;
+    const char* filename;
+};
+
+enum
+{
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE
+};
+
+struct token
+{
+    int type;
+    int flags;
+
+    union 
+    {
+       char cval;
+       const char* sval;
+       unsigned int inum;
+       unsigned long lnum;
+       unsigned long long llnum;
+       void* any;
+    };
+
+    // True if their is whitespace betweeen the token and the next token
+    bool whitespace;
+    
+    //(5+10) all tokens "5", "+", and "10" points at the firts bracket "8"
+    const char* between_brackets;
+
+
+};
 
 enum
 {

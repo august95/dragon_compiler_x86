03ce5eb771c69dceee6240b2d19ede41883b756c
august95 <ingvar.wedervang@gmail.com>
2023-05-05 22:33:16 +0200

    started initializing of datatypes, functions for verifying double datatype added e.g. long long

--- compiler.h
+++ compiler.h
@@ -295,9 +295,17 @@ struct datatype
 
 enum
 {
-    DATATYPE_EXPECT_PRIMITIVE,
-    DATATYPE_EXPECT_UNION,
-    DATATYPE_EXPECT_STRUCT
+    DATA_TYPE_EXPECT_PRIMITIVE,
+    DATA_TYPE_EXPECT_UNION,
+    DATA_TYPE_EXPECT_STRUCT
+};
+enum
+{
+    DATA_SIZE_ZERO = 0,
+    DATA_SIZE_BYTE = 1,
+    DATA_SIZE_WORD = 2,
+    DATA_SIZE_DWORD = 3,
+    DATA_SIZE_DDWORD = 4
 };
 int compile_file(const char *filename, const char *out_file, int flags);
 struct compile_process *compile_process_create(const char *filename, const char *filename_out, int flags);

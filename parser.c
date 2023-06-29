dcf2acac43b3af3da8ac4c637392f9c9edc0a2b7
august95 <ingvar.wedervang@gmail.com>
2023-06-29 20:52:26 +0200

    int, float and double datatypes are parsed by the parser

--- parser.c
+++ parser.c
@@ -457,10 +457,30 @@ void parse_datatype(struct datatype* dtype)
     parse_datatype_type(dtype);
     parse_datatype_modifiers(dtype);
 }
+
+bool parse_is_int_valid_after_datatype(struct datatype* dtype)
+{
+    return dtype->type == DATA_TYPE_LONG || dtype->type == DATA_TYPE_FLOAT || dtype->type == DATA_TYPE_DOUBLE;
+}
+
+void parser_ignore_int(struct datatype* dtype)
+{
+    if(!token_is_keyword(token_peek_next(), "int"))
+    {
+        return;
+    }
+
+    if(!parse_is_int_valid_after_datatype(dtype))
+    {
+        compiler_error(current_process, "invlid int provided");
+    }
+}
 void parse_variable_function_or_struct_union(struct history *history)
 {
     struct datatype dtype;
     parse_datatype(&dtype);
+
+    parser_ignore_int(&dtype);
 }
 
 void parse_keyword(struct history *history)

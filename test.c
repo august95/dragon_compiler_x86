6ddd2caef1fac85ddf8e7c1a002f07ff974a210c
Ingvar August Wedervang <ingvar.wedervang@gmail.com>
2025-02-03 18:58:00 +0100

    updated test file to test for  loops, function calls and external printf call

--- test.c
+++ test.c
@@ -1,7 +1,8 @@
-//int printf(const char *format, ...);
+int printf(const char *format, ...);
 
 int test(int a)
 {
+  printf("hello world!\n");
   return a + 1;
 }
 
@@ -9,4 +10,11 @@ int test(int a)
 int main()
 {
     test(30);
+
+    int a = 0;
+    for( a = 0; a < 10 ;  a = a + 1)
+    {
+      test(a);     
+
+    }
 }
